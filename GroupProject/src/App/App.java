3e9c89f78bd6f3a74b259fb046e3a22373cfada3
lau <lau@samlau>
2024-12-06 12:33:29 +0800

    Java Project

--- GroupProject/src/App/App.java
+++ GroupProject/src/App/App.java
@@ -3,8 +3,9 @@ import java.awt.AWTException;
 
 public class App {
 
-	public static void main(String[] args) throws AWTException {
+	public static void main(String[] args) throws AWTException, InterruptedException {
 		System.out.println("This is the java file that run the main game.");
+		Thread.sleep(2000);
 		KBEvent.clean_console();
 	}
 
--- GroupProject/src/App/KBEvent.java
+++ GroupProject/src/App/KBEvent.java
@@ -5,14 +5,14 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class KBEvent {
-	public static void clean_console() throws AWTException {
+	public static void clean_console() throws AWTException, InterruptedException {
 		Robot robot = new Robot();
-		robot.delay(2000);
-		robot.keyPress(KeyEvent.VK_SHIFT);
-		robot.keyPress(KeyEvent.VK_F10);
-		robot.keyPress(KeyEvent.VK_R);
-		robot.keyRelease(KeyEvent.VK_SHIFT);
-		robot.keyRelease(KeyEvent.VK_F10);
-		robot.keyRelease(KeyEvent.VK_R);
+        //Clean the console
+        robot.keyPress(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_F10);
+        robot.keyRelease(KeyEvent.VK_SHIFT);
+        robot.keyRelease(KeyEvent.VK_F10);
+        robot.keyPress(KeyEvent.VK_R);
+        robot.keyRelease(KeyEvent.VK_R);
 	}
 }
