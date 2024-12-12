110173ba31dcad0f90be31578b4e75d99d7c093e
A <A@MSI>
2024-12-12 23:03:07 +0800

    Update App class
    1. Ensure that the user is prompted repeatedly for their player name
    until a valid name is provided (non-empty input).

--- GroupProject/src/App/App.java
+++ GroupProject/src/App/App.java
@@ -14,9 +14,10 @@ public class App {
 		Scanner choiceScanner = new Scanner(System.in);
 		Scanner nameScanner = new Scanner(System.in);
 		boolean developChoice = false;
+		String playerName;
 		Message.displayDifficultyMenu();
 		do {
-			System.out.print(ColorPlate.magenta + ColorPlate.bold+"Your choice: " + ColorPlate.resetColor);
+			System.out.print(ColorPlate.magenta + ColorPlate.bold + "Your choice: " + ColorPlate.resetColor);
 			choice = choiceScanner.nextLine();
 			System.out.println();
 
@@ -28,10 +29,15 @@ public class App {
 				Message.print_letter_by_letter(
 						"\nPlease enter your character's name, and get ready to face the challenge!\n", 25);
 				Thread.sleep(500);
-				Message.print_letter_by_letter("Your character's name is: ", 25);
-				Thread.sleep(500);
-				String playerName = nameScanner.nextLine();
-				System.out.println();
+
+				do {
+
+					Message.print_letter_by_letter("Your character's name is: ", 25);
+					Thread.sleep(500);
+					playerName = nameScanner.nextLine();
+					System.out.println();
+				} while (playerName.length() <= 0);
+
 				Message.print_letter_by_letter("Good luck, " + playerName + "! Your adventure is about to begin. ", 25);
 				Thread.sleep(500);
 				System.out.println();
@@ -57,7 +63,7 @@ public class App {
 				System.out.println(
 						ColorPlate.red + "You have selected Extreme Mode. This feature is still under development."
 								+ ColorPlate.resetColor);
-				
+
 				developChoice = true;
 				break; // Exit the switch after handling choice 3
 			default:
@@ -65,7 +71,8 @@ public class App {
 				System.out.println(ColorPlate.red + "Invalid choice. Please enter 1, 2, or 3." + ColorPlate.resetColor);
 				break; // Exit the switch for invalid input
 			}
-		} while (choice !="1" || choice !="2" || choice !="3" || developChoice== true); // Continue until a valid choice is made
+		} while (choice != "1" || choice != "2" || choice != "3" || developChoice == true); // Continue until a valid
+																							// choice is made
 
 	}
 }
